497c73e3c4d4a7477b73692ea619497aaeb99ad1
agnesgrd <agnesguinard30@gmail.com>
2025-02-27 15:54:23 +0100

    Add interpolation in data preprocessing

--- callbacks/utils/preprocessing_utils.py
+++ callbacks/utils/preprocessing_utils.py
@@ -1,5 +1,7 @@
 from math import *
 import static.constants as c
+from pathlib import Path
+import pickle
 
 def update_chunk_limits(total_duration):
     # Define chunk duration (in seconds) and total duration
@@ -15,4 +17,39 @@ def update_chunk_limits(total_duration):
         
     # Return chunk limits in a dictionary to store in the dcc.Store
     return chunk_limits
+
+# tentative function to interpolate missing channels using mne 
+def interpolate_missing_channels(raw):
+	print("interpolate missing or bad channel")
+	
+    # open a file containing the good 274 channels
+	with open(Path.cwd() / "model_pipeline/good_channels", 'rb') as fp:
+		good_channels = pickle.load(fp)
+
+	with open(Path.cwd() / "model_pipeline/loc_meg_channels.pkl", 'rb') as fp: #path to the file.pkl containing for each channel name its location
+		loc_meg_channels = pickle.load(fp)
+		
+	existing_channels = raw.info['ch_names'] # returns the list of chanel names that are present in the data
+	missing_channels = list(set(good_channels) - set(existing_channels)) # gets the list of missing channels by comparing the existing channel names with the list of good channels
+	new_raw = raw.copy() 
+
+	# creates fake channels and set them to "bad channels", rename them with the name of the missing channels, 
+	#then mne is supposed to be able to reconstruct bad channels with "interpolate_bads" 
+	for miss in missing_channels:
+		to_copy = raw.info['ch_names'][50] #pick a random channel
+		new_channel = raw.copy().pick([to_copy])
+		new_channel.rename_channels({to_copy: miss})
+		new_raw.add_channels([new_channel], force_update_info=True)
+
+		#specifies the location of the missing channel
+		for i in range(len(new_raw.info['chs'])):
+			if new_raw.info['chs'][i]['ch_name'] == miss:
+				new_raw.info['chs'][i]['loc'] = loc_meg_channels[miss]
+			
+	new_raw.reorder_channels(good_channels)
+	new_raw.info['bads'] = missing_channels
+
+	new_raw.interpolate_bads(origin=(0, 0, 0.04),reset_bads=True) 
+
+	return new_raw
 
